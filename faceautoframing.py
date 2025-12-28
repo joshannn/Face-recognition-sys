e27e7f7aae8dc1d176641a11c0e06d0d81693b5f
joshan <dhakaljoshan@gmail.com>
2025-12-28 18:48:33 +0545

    Add files via upload

---
+++ faceautoframing.py
@@ -0,0 +1,37 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")  # lightweight
+
+cap = cv2.VideoCapture(0)
+FRAME_W, FRAME_H = 640, 480
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, conf=0.4, classes=[0])  # class 0 = person
+
+    if results[0].boxes:
+        box = results[0].boxes.xyxy[0]
+        x1, y1, x2, y2 = map(int, box)
+
+        cx = (x1 + x2) // 2
+        cy = (y1 + y2) // 2
+
+        crop_w, crop_h = 400, 400
+
+        x1 = max(0, cx - crop_w // 2)
+        y1 = max(0, cy - crop_h // 2)
+        x2 = min(frame.shape[1], cx + crop_w // 2)
+        y2 = min(frame.shape[0], cy + crop_h // 2)
+
+        frame = cv2.resize(frame[y1:y2, x1:x2], (FRAME_W, FRAME_H))
+
+    cv2.imshow("Auto Framing (YOLO)", frame)
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ facedetector.py
@@ -0,0 +1,15 @@
+import cv2
+face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+video_cap=cv2.VideoCapture(0)
+while True:
+    ret, video=video_cap.read()
+    col=cv2.cvtColor(video, cv2.COLOR_BGR2GRAY)
+    
+    faces=face_cascade.detectMultiScale(col, scaleFactor=1.1, minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(video, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow("video_live", video)
+    if cv2.waitKey(10)==ord("q"):
+        break
+video_cap.release()
+cv2.destroyAllWindows()
